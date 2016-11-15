9e0340adc0f0c937c8d46b1e20da618bd1d5e0fb
M Galang Arbi S <galangarbis@gmail.com>
2016-11-15 21:35:03 +0700

    route express

--- express.js
+++ express.js
@@ -5,3 +5,7 @@ var app=express();
 app.listen(3000, function() {
    console.log('Server is running on port 3000'); 
 });
+//learn Route with express
+app.get('/',function(req,res) {
+    res.send("Hello from Express JS");
+});
